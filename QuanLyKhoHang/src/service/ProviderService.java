4d62a4d3d360e030ffff6cc64c5b3d115365c039
hungpt58nd <hungpt58.uet@gmail.com>
2018-10-15 20:52:47 +0700

    udpate

--- QuanLyKhoHang/src/service/ProviderService.java
+++ QuanLyKhoHang/src/service/ProviderService.java
@@ -3,17 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package quanlykhohang.model;
+package service;
 
 /**
  *
  * @author Storm Spirit
  */
-public class PersonEntity {
-    public String name;
-    public String address;
-    public String phone;
-    public String createdAt;
-    public int total;
-    public String note;
+public class ProviderService {
+    
 }
